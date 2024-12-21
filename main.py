080c603405a21461eb64b2f1c49e47be35928922
ishaaq363 <imatovu383@gmail.com>
2024-12-21 18:44:21 +0200

    fuctions in py

---
+++ main.py
@@ -0,0 +1,30 @@
+# def display():
+#     print("Hello My Name Is Is-haaq")
+
+# def display(name,age):
+#     print("hello my name is", name," and i am",age,"days old")
+
+
+# name=input("enter your name")
+# age=int(input("Enter your age"))
+# display(name,age)
+
+def sum(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+num1=int(input("enter no 1"))
+num2=int(input("enter no 2"))
+
+print("the addition of two no is" ,sum(num1,num2))
+print("the difference of two no is" ,sub(num1,num2))
+print("the product of two no is" ,mul(num1,num2))
+print("the quotient of two no is" div,(num1,num2))
